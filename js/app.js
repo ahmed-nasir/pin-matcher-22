e7524e86eabfc558fa9238950149666807d14a6b
Nasir Ahmed <nasirahmed595@gmail.com>
2022-02-14 23:56:17 +0600

    pin matched

--- js/app.js
+++ js/app.js
@@ -11,3 +11,36 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    } else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+});
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+
+    const successNotification = document.getElementById('notify-success');
+
+    const errorNotification = document.getElementById('notify-fail');
+
+    if (pin == typedNumbers) {
+        successNotification.style.display = 'block';
+        errorNotification.style.display = 'none';
+    } else {
+        successNotification.style.display = 'none';
+        errorNotification.style.display = 'block';
+    }
+
+}
